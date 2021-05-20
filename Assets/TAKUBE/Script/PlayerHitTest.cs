5c927049b00c15f05576ebbdeddf81a0b064b1e0
okuzawa-reiya <okuzawa_reiya@yahoo.co.jp>
2021-05-20 15:43:48 +0900

    段差の上に矢印ブロックがあった場合、矢印の方向関係なく登れてしまう問題を解消

--- Assets/TAKUBE/Script/PlayerHitTest.cs
+++ Assets/TAKUBE/Script/PlayerHitTest.cs
@@ -33,7 +33,7 @@ public class PlayerHitTest : MonoBehaviour
         string tag = other.transform.tag;
         if (tag == "ClimbBlock"||tag=="GimicClearBlock"||tag == "Block")
         {
-            Vector3 ray_pos = other.transform.position + Vector3.down * 0.1f + Vector3.right * 0.38f;
+            Vector3 ray_pos = other.transform.position + Vector3.down * 0.1f + Vector3.back * 0.38f;
             Ray ray = new Ray(ray_pos, Vector3.up);
             if (!Physics.Raycast(ray, out RaycastHit hit, other.transform.lossyScale.y))
             {
@@ -55,7 +55,6 @@ public class PlayerHitTest : MonoBehaviour
                             return;
                         }
                     }
-
                     if (tag == "GimicOneWayBlock")
                     {
                         if ((hit.transform.GetComponent<OneWayCenter>().m_Direction == OneWayCenter.DIRECTION.LEFT && dir != COLLISIONDIRECTION.RIGHT) ||
