cb3fdcfe6db3a011153c1f77da9c788d214464d3
nekomu <nishiko.245.sao@gmail.com>
2021-05-30 01:20:23 +0900

    落下しながらゴール修正完全版

--- Assets/Nishiko/framerate.cs
+++ Assets/Nishiko/framerate.cs
@@ -14,6 +14,9 @@ public class framerate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
